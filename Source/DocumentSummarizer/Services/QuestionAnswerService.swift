f0262989da990dd98c06ac48746223e6793d37d9
John Cruz <johnamcruz@gmail.com>
2023-10-09 22:54:16 -1000

    remove large model

---
+++ Source/DocumentSummarizer/Services/QuestionAnswerService.swift
@@ -0,0 +1,21 @@
+//
+//  QuestionAnswerable.swift
+//  DocumentSummarizer
+//
+//  Created by John M Cruz on 10/9/23.
+//
+
+import Foundation
+import CoreML
+import Tokenizers
+import Models
+
+protocol QuestionAnswerServiceable {
+    
+}
+
+class QuestionAnswerService {
+    func question(input: String, question: String) async throws -> String {
+        ""
+    }
+}
