22017c8dfe2136a4c6a0f0499b914a88b07ce471
John Cruz <johnamcruz@gmail.com>
2023-10-10 15:11:44 -1000

    updated height

--- Source/DocumentSummarizer/Views/ChatBubbleView.swift
+++ Source/DocumentSummarizer/Views/ChatBubbleView.swift
@@ -9,32 +9,40 @@ import SwiftUI
 
 struct ChatBubbleView: View {
     let message: Message
+    @State var height: CGFloat = 120
     
     var body: some View {
-        HStack {
+        HStack(alignment: .top) {
             Image(systemName: Images.person)
                 .resizable()
                 .frame(width: 40, height: 40, alignment: .center)
                 .cornerRadius(20)
-            VStack(alignment: .leading) {
-                UnevenRoundedRectangle(cornerRadii: .init(
-                    topLeading: 10.0,
-                    bottomLeading: 10.0,
-                    bottomTrailing: 10.0,
-                    topTrailing: 10.0))
-                .foregroundStyle(Color(red: 0.13, green: 0.21, blue: 0.29))
-                .overlay {
-                    Text(message.message)
-                        .lineLimit(nil)
-                        .multilineTextAlignment(.center)
-                        .padding()
+                .padding(EdgeInsets(top: 7, leading: 0, bottom: 0, trailing: 0))
+            GeometryReader { reader in
+                VStack(alignment: .leading) {
+                    UnevenRoundedRectangle(cornerRadii: .init(
+                        topLeading: 10.0,
+                        bottomLeading: 10.0,
+                        bottomTrailing: 10.0,
+                        topTrailing: 10.0))
+                    .foregroundStyle(Color(red: 0.13, green: 0.21, blue: 0.29))
+                    .overlay {
+                        Text(message.message)
+                            .lineLimit(nil)
+                            .multilineTextAlignment(.center)
+                            .padding()
+                    }
+                    Text(message.user)
+                        .font(.footnote)
+                        .foregroundColor(.gray)
                 }
-                Text(message.user)
-                    .font(.footnote)
-                    .foregroundColor(.gray)
+                .onAppear {
+                    height = reader.size.height
+                }
             }
         }
-        .padding()
+        .frame(height: height)
+        .padding(5)
     }
 }
 
