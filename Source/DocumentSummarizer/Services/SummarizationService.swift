c492b73127f472ea01ef13cab8a7d457be84d99e
John Cruz <johnamcruz@gmail.com>
2023-10-11 22:29:37 -1000

    updated

--- Source/DocumentSummarizer/Services/SummarizationService.swift
+++ Source/DocumentSummarizer/Services/SummarizationService.swift
@@ -34,8 +34,8 @@ class SummarizationService: SummarizationServiceable {
     func summarize(input: String) async throws -> String {
         let inputIds = try generateInputIds(input: input)
         let encoderModel = try BARTencoderModel()
-        let attentionMasks = try generateAttentionMask(inputIds: inputIds)
-        let inputParameter = BARTencoderModelInput(input_ids: attentionMasks,
+        let attentionMasks = try MLMultiArray(shape: [1,128], dataType: .int32)
+        let inputParameter = BARTencoderModelInput(input_ids: MLMultiArray.from(inputIds, dims: 2),
                                                    attention_mask: attentionMasks)
         let output = try await encoderModel.prediction(input: inputParameter)
         let result = decodeOutput(output: output.last_hidden_state.toIntArray())
@@ -49,15 +49,5 @@ class SummarizationService: SummarizationServiceable {
     
     func decodeOutput(output: [Int])-> String {
         return tokenizer.decode(tokens: tokenizer.stripBOS(tokens: tokenizer.stripEOS(tokens: output)))
-    }
-    
-    func generateAttentionMask(inputIds: [Int]) throws -> MLMultiArray {
-        let attentionMask = try MLMultiArray(shape: [1, NSNumber(integerLiteral: inputIds.count)], dataType: .float32)
-        
-        // Iterate over the input_ids and set the corresponding values in the MLMultiArray to 1.0.
-        for index in 0..<inputIds.count {
-            attentionMask[index] = inputIds[index] == 0 ?  0.0 : 1.0
-        }
-        return attentionMask
     }
 }
