7b53d8a735dbbbfb93e0a51570ed27eaf7520c98
John Cruz <johnamcruz@gmail.com>
2023-10-14 15:34:52 -1000

    fix input

--- Source/DocumentSummarizer/Services/SummarizationService.swift
+++ Source/DocumentSummarizer/Services/SummarizationService.swift
@@ -34,8 +34,9 @@ class SummarizationService: SummarizationServiceable {
     func summarize(input: String) async throws -> String {
         let inputIds = try generateInputIds(input: input)
         let encoderModel = try BARTencoderModel()
+        print(encoderModel.model.modelDescription)
         let attentionMasks = try MLMultiArray(shape: [1,128], dataType: .int32)
-        let inputParameter = BARTencoderModelInput(input_ids: MLMultiArray.from(inputIds, dims: 2),
+        let inputParameter = BARTencoderModelInput(input_ids: try inputIds.toMultiArray(),
                                                    attention_mask: attentionMasks)
         let output = try await encoderModel.prediction(input: inputParameter)
         let result = decodeOutput(output: output.last_hidden_state.toIntArray())
@@ -49,5 +50,21 @@ class SummarizationService: SummarizationServiceable {
     
     func decodeOutput(output: [Int])-> String {
         return tokenizer.decode(tokens: tokenizer.stripBOS(tokens: tokenizer.stripEOS(tokens: output)))
+    }
+}
+
+extension [Int] {
+    func toMultiArray() throws -> MLMultiArray {
+        let inputArray = try MLMultiArray(shape: [1, 128], dataType: .float32)
+
+        for i in 0..<self.count {
+            inputArray[i] = NSNumber(floatLiteral: Double(self[i]))
+        }
+
+        for i in self.count..<128 {
+            inputArray[i] = NSNumber(floatLiteral: 0.0)
+        }
+
+        return inputArray
     }
 }
