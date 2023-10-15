e105921266f224d6a9afd154d634801068165d38
John Cruz <johnamcruz@gmail.com>
2023-10-14 15:45:50 -1000

    added comments

--- Source/DocumentSummarizer/Services/SummarizationService.swift
+++ Source/DocumentSummarizer/Services/SummarizationService.swift
@@ -13,12 +13,61 @@ import Models
 enum SummarizationError: Error {
     case invalidLanguageModel
     case resourceFileMissing
+    case invalidResult
 }
 
 protocol SummarizationServiceable {
     func summarize(input: String) async throws -> String
 }
+/*
+ **High-level algorithm for BART summarization using Hugging Face encoder and decoder:**
 
+ 1. **Load the encoder and decoder:**
+ ```
+ from transformers import BartEncoder, BartDecoder, AutoModelForSeq2SeqLM
+
+ bart_encoder = BartEncoder.from_pretrained("facebook/bart-base")
+ bart_decoder = BartDecoder.from_pretrained("facebook/bart-base")
+ # or
+ bart_model = AutoModelForSeq2SeqLM.from_pretrained("facebook/bart-base")
+ ```
+
+ 2. **Encode the input text:**
+ ```
+ input_ids = bart_encoder.encode(input_text)
+ ```
+
+ 3. **Generate the summary:**
+ ```
+ # If using the Hugging Face AutoModelForSeq2SeqLM:
+ summary_ids = bart_model.generate(
+     input_ids=input_ids,
+     max_length=summary_length,
+ )
+
+ # If using the Hugging Face BartEncoder and BartDecoder:
+ summary_ids = bart_decoder.generate(
+     encoder_outputs=bart_encoder.predict(input_ids),
+     max_length=summary_length,
+ )
+ ```
+
+ 4. **Decode the summary:**
+ ```
+ summary_text = bart_encoder.decode(summary_ids)
+ ```
+
+ 5. **Return the summary:**
+ ```
+ return summary_text
+ ```
+
+ **Additional notes:**
+
+ * You can adjust the `summary_length` parameter to control the length of the summary.
+ * You can also use beam search to generate more diverse summaries. To do this, set the `num_beams` parameter to a value greater than 1.
+ * If you are using the Hugging Face AutoModelForSeq2SeqLM, you can also use the `eos_token_id` parameter to specify the end-of-sequence token. This is useful if you want to generate summaries that are a specific length.
+ */
 class SummarizationService: SummarizationServiceable {
     let tokenizer: Tokenizer
     
